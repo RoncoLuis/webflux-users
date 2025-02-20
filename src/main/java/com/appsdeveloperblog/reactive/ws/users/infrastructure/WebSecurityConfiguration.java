0b0b90d44babe07a0831cbbfa493c34c8995c83e
roncoluis <w580ix@gmail.com>
2025-02-19 21:16:46 -0600

    Enable spring security configuration

---
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.appsdeveloperblog.reactive.ws.users.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration //this annotation is equivalent to XML configuration file. Contains a source of bean definitions
+@EnableWebFlux
+public class WebSecurityConfiguration {
+    //when you add spring security to your application all your endpoints will be protected and will require authentication
+
+    //Configuration to indicate which endpoints should not require authentication
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.POST, "/users")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable).build();
+    }
+}
